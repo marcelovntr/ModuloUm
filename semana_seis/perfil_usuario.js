4dbe0ffa9f5f4b5cbef1ebb828a6cd08754a9cfd
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-02-28 02:22:11 -0300

    alterações HTML, CSS e JS com manipulação do DOM

--- semana_seis/perfil_usuario.js
+++ semana_seis/perfil_usuario.js
@@ -1,1 +1,12 @@
 alert('Seja muito bem vindo ao seu perfil de usuário!');
+
+function alteraNome (){
+    const alterarNome = document.getElementById('nome');
+
+    let novoNome = prompt('Qual o novo nome?');
+    
+    if(novoNome===''|| novoNome === null){
+        novoNome='Nome Inicial(teste)';
+    }
+    alterarNome.innerText = novoNome;
+}
